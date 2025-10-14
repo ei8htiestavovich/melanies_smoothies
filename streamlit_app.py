715f65a541c286baf4c07bb6b6f336ad4e06241e
ei8htiestavovich <ei8hties_tavovich@outlook.com>
2025-10-14 17:03:59 -0600

    Update streamlit_app.py
    try something else

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
-warehouse = st.secrets['connections.snowflake']['warehouse']
-session.sql(f'USE WAREHOUSE "{warehouse}"').collect()
+#warehouse = st.secrets['connections.snowflake']['warehouse']
+#session.sql(f'USE WAREHOUSE "{warehouse}"').collect()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
